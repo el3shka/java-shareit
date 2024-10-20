ad6ae8b720520e46739ddd12122ff28bebade80b
el3shka <harv3st3r666@gmail.com>
2024-10-20 18:59:42 +0600

    sprint-15

--- src/main/java/ru/practicum/shareit/user/exception/ConflictException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.user.exception;
-
-public class ConflictException extends RuntimeException {
-    public ConflictException(String message) {
-        super(message);
-    }
-}
