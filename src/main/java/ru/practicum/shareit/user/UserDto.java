1bd8ff8f216547b9f720f1ab2ba4b0fb38d0510f
el3shka <harv3st3r666@gmail.com>
2024-10-21 00:31:18 +0600

    sprint-15

--- src/main/java/ru/practicum/shareit/user/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/UserDto.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.practicum.shareit.user.markers.Create;
-import ru.practicum.shareit.user.markers.Update;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -15,7 +14,8 @@ public class UserDto {
     private Long id;
     @NotBlank(groups = {Create.class})
     private String name;
-    @Email(groups = {Create.class, Update.class})
+    //@Email(groups = {Create.class, Update.class})
+    @Email(groups = {Create.class})
     @NotBlank(groups = {Create.class})
     private String email;
 }
